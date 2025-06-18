45a8bfd56ced1b32c91fbb40725f741c5a00636d
nastyavav <vavrinovicha@gmail.com>
2025-06-18 11:15:12 +0300

    добавление валидации в USSView

--- src/views/USSView/USSView.vue
+++ src/views/USSView/USSView.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
+import { helpers, required } from '@vuelidate/validators';
+
+import { getMessageRequired } from '@/utils/getMessage';
+
 import type { DateModel } from '@/types/datePeriod.types';
 
 import {
     DATE_FNS_FORMAT_ISO_WITH_TIMEZONE,
     DATE_FORMAT_ISO_WITH_TIMEZONE,
 } from '@/constants/dates';
+import { exampleList } from '@/constants/example';
 import { ROUTE_NAME_MAIN } from '@/constants/routeNames';
+
+import { useValidation } from '@/composable/useValidation';
 
 const LABEL_PERIOD="Период"
 
@@ -17,7 +24,25 @@ const formModel = ref<FromModel>({
     period: null,
 });
 
-function onSubmit() {
+const validationRules = computed(() => ({
+    period: {
+        required: helpers.withMessage(getMessageRequired(LABEL_PERIOD), required),
+    },
+}));
+
+const { formValidation, validateForm } = useValidation(formModel, validationRules);
+
+async function onShow() {
+    const isValid = await validateForm();
+    if (!isValid) return;
+
+    console.log(formModel.value);
+}
+
+async function onPrepare() {
+    const isValid = await validateForm();
+    if (!isValid) return;
+
     console.log(formModel.value);
 }
 </script>
@@ -33,12 +58,15 @@ function onSubmit() {
         :format-date="DATE_FNS_FORMAT_ISO_WITH_TIMEZONE"
         :format-dayjs="DATE_FORMAT_ISO_WITH_TIMEZONE"
         :label="LABEL_PERIOD"
+        :error-message="formValidation.period.errorMessage"
+        :error="formValidation.period.invalid"
+        :required="formValidation.period.required"
         placeholder="Выберите период"
         name="date"
         />
         <HorizontalList>
-        <BaseButton>Посмотреть</BaseButton>
-        <BaseButton>Подготовить к загрузке</BaseButton>
+            <BaseButton @click="onShow"> Посмотреть </BaseButton>
+            <BaseButton @click="onPrepare"> Подготовить к загрузке </BaseButton>
         </HorizontalList>
     </BaseForm>
     </TitledContent>

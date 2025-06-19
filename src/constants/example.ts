05172773a68ff23546a60bea900398a8a4dd538a
Vfif <gmariya6666@gmail.com>
2025-06-19 15:59:50 +0300

    Обновлен компонент AnalysisTaskStatusChanges. Добавлен список projectsList для выбора проекта

--- src/constants/example.ts
+++ src/constants/example.ts
@@ -1,6 +1,10 @@
 export const exampleList = [
   { id: 1, name: 'Alice', age: 30 },
   { id: 2, name: 'Bob', age: 25 },
-
   { id: 3, name: 'Charlie', age: 35 },
+];
+
+export const projectsList = [
+  { id: 1, name: 'Кредитные программы' },
+  { id: 2, name: 'Цифровизация услуг' },
 ];

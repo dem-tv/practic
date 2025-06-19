29a41d1d1243efaf29ec3f1741b5b095718646f8
Vfif <gmariya6666@gmail.com>
2025-06-19 11:33:15 +0300

    Обновлен компонент AccountingByDepartmentsView. Добавлен массив typeProjectsList для выбора типа проекта

--- src/constants/example.ts
+++ src/constants/example.ts
@@ -1,6 +1,11 @@
 export const exampleList = [
   { id: 1, name: 'Alice', age: 30 },
   { id: 2, name: 'Bob', age: 25 },
-
   { id: 3, name: 'Charlie', age: 35 },
+];
+
+export const typeProjectsList = [
+  { id: 1, name: 'Инвестиционные проекты' },
+  { id: 2, name: 'Кредитные программы' },
+  { id: 3, name: 'Цифровизация услуг' },
 ];

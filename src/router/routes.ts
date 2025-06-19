3d69c7c4fd88ef309d6152e22e42701b29320127
Vfif <gmariya6666@gmail.com>
2025-06-19 13:32:17 +0300

    Реализованы новые роуты в SupportView. Добавлены пустые компоненты

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -11,6 +11,21 @@ import {
   ROUTE_NAME_MAIN,
   ROUTE_NAME_QMS,
   ROUTE_NAME_SUPPORT,
+  ROUTE_NAME_ANALYSIS_TASK_STATUS_CHANGES,
+  ROUTE_NAME_DONE_TASKS_BY_PERIOD,
+  ROUTE_NAME_EXTRA_TASKS_BY_DATE,
+  ROUTE_NAME_ANALYSIS_TASKS,
+  ROUTE_NAME_TASKS_RETURNED_AND_RECEIVED_FOR_ONE_PERIOD,
+  ROUTE_NAME_ANALYSIS_TASK_STATUS_CHANGESTASKS_RETURNED_AND_RECEIVED_AS_UNFULFILLED_FOR_ONE_PERIOD,
+  ROUTE_NAME_TASKS_RETURNED_FOR_REVISION_BY_PERIOD,
+  ROUTE_NAME_TASKS_EXTRA_PERIOD,
+  ROUTE_NAME_DEADLINE_CONTROL_SUPPORT_TASKS_EXECUTION,
+  ROUTE_NAME_UNFULFILLED_REQUESTS_FOR,
+  ROUTE_NAME_UNFULFILLED_REQUESTS_ON_APPRECIATED_AND_UNAPPRECIATED,
+  ROUTE_NAME_ACT_OF_SUPPORT_BIB,
+  ROUTE_NAME_TASKS_EXTRA_BY_DATE,
+  ROUTE_NAME_TESTING,
+  ROUTE_NAME_RECEIVED_TASKS_BY_PERIOD,
   ROUTE_NAME_ACCOUNTING_PROJECT_LIST,
   ROUTE_NAME_ACCOUNTING_2,
   ROUTE_NAME_ACCOUNTING_BY_DEPARTMENTS,
@@ -37,6 +52,22 @@ const TestAnalysisView = () => import('@/views/TestAnalysisView/TestAnalysisView
 const PerformanceEvaluationView = () => import('@/views/PerformanceEvaluationView/PerformanceEvaluationView.vue');
 const EffectivenessProcessView = () => import('@/views/EffectivenessProcessView/EffectivenessProcessView.vue');
 
+//Сопровождение
+const AnalysisTaskStatusChanges = () => import('@/views/AnalysisTaskStatusChanges/AnalysisTaskStatusChanges.vue');
+const DoneTasksByPeriod = () => import('@/views/DoneTasksByPeriod/DoneTasksByPeriod.vue');
+const ExtraTasksByDate = () => import('@/views/ExtraTasksByDate/ExtraTasksByDate.vue');
+const AnalysisTasks = () => import('@/views/AnalysisTasks/AnalysisTasks.vue');
+const TasksReturnedAndReceivedForOnePeriod = () => import('@/views/TasksReturnedAndReceivedForOnePeriod/TasksReturnedAndReceivedForOnePeriod.vue');
+const TasksReturnedAndReceivedAsUnfulfilledForOnePeriod = () => import('@/views/TasksReturnedAndReceivedAsUnfulfilledForOnePeriod/TasksReturnedAndReceivedAsUnfulfilledForOnePeriod.vue');
+const TasksReturnedForRevisionByPeriod = () => import('@/views/TasksReturnedForRevisionByPeriod/TasksReturnedForRevisionByPeriod.vue');
+const TasksExtraPeriod = () => import('@/views/TasksExtraPeriod/TasksExtraPeriod.vue');
+const DeadlineControlSupportTasksExecution = () => import('@/views/DeadlineControlSupportTasksExecution/DeadlineControlSupportTasksExecution.vue');
+const UnfulfilledRequestsFor = () => import('@/views/UnfulfilledRequestsFor/UnfulfilledRequestsFor.vue');
+const UnfulfilledRequestsOnAppreciatedAndUnappreciated = () => import('@/views/UnfulfilledRequestsOnAppreciatedAndUnappreciated/UnfulfilledRequestsOnAppreciatedAndUnappreciated.vue');
+const ActOfSupportBib = () => import('@/views/ActOfSupportBib/ActOfSupportBib.vue');
+const TasksExtraByDate = () => import('@/views/TasksExtraByDate/TasksExtraByDate.vue');
+const Testing = () => import('@/views/Testing/Testing.vue');
+const ReceivedTasksByPeriod = () => import('@/views/ReceivedTasksByPeriod/ReceivedTasksByPeriod.vue');
 
 
 export const routes: Readonly<RouteRecordRaw[]> = [
@@ -114,6 +145,84 @@ export const routes: Readonly<RouteRecordRaw[]> = [
     path: '/effectiveness-process-support',
     name: ROUTE_NAME_EFFECTIVENESS_PROCESS,
     component: EffectivenessProcessView,
+  },
+
+
+  //Сопровождение
+  {
+    path: '/analysis-task-status-changes',
+    name: ROUTE_NAME_ANALYSIS_TASK_STATUS_CHANGES,
+    component: AnalysisTaskStatusChanges,
+  },
+  {
+    path: '/done-tasks-by-period',
+    name:   ROUTE_NAME_DONE_TASKS_BY_PERIOD,
+    component: DoneTasksByPeriod,
+  },
+  {
+    path: '/extra-tasks-by-date',
+    name: ROUTE_NAME_EXTRA_TASKS_BY_DATE,
+    component: ExtraTasksByDate,
+  },
+  {
+    path: '/analysis-tasks',
+    name: ROUTE_NAME_ANALYSIS_TASKS,
+    component: AnalysisTasks,
+  },
+  {
+    path: '/tasks-returned-and-received-for-one-period',
+    name: ROUTE_NAME_TASKS_RETURNED_AND_RECEIVED_FOR_ONE_PERIOD,
+    component: TasksReturnedAndReceivedForOnePeriod,
+  },
+  {
+    path: '/tasks-returned-and-received-as-unfulfilled-for-one-period',
+    name: ROUTE_NAME_ANALYSIS_TASK_STATUS_CHANGESTASKS_RETURNED_AND_RECEIVED_AS_UNFULFILLED_FOR_ONE_PERIOD,
+    component: TasksReturnedAndReceivedAsUnfulfilledForOnePeriod,
+  },
+  {
+    path: '/tasks-returned-for-revision-by-period',
+    name: ROUTE_NAME_TASKS_RETURNED_FOR_REVISION_BY_PERIOD,
+    component: TasksReturnedForRevisionByPeriod,
+  },
+  {
+    path: '/tasks-extra-period',
+    name: ROUTE_NAME_TASKS_EXTRA_PERIOD,
+    component: TasksExtraPeriod,
+  },
+  {
+    path: '/deadline-control-support-tasks-execution',
+    name: ROUTE_NAME_DEADLINE_CONTROL_SUPPORT_TASKS_EXECUTION,
+    component: DeadlineControlSupportTasksExecution,
+  },
+  {
+    path: '/unfulfilled-requests-for',
+    name: ROUTE_NAME_UNFULFILLED_REQUESTS_FOR,
+    component: UnfulfilledRequestsFor,
+  },
+  {
+    path: '/unfulfilled-requests-on-appreciated-and-unappreciated',
+    name: ROUTE_NAME_UNFULFILLED_REQUESTS_ON_APPRECIATED_AND_UNAPPRECIATED,
+    component: UnfulfilledRequestsOnAppreciatedAndUnappreciated,
+  },
+  {
+    path: '/act-of-support-bib',
+    name: ROUTE_NAME_ACT_OF_SUPPORT_BIB,
+    component: ActOfSupportBib,
+  },
+  {
+    path: '/tasks-extra-by-date',
+    name: ROUTE_NAME_TASKS_EXTRA_BY_DATE,
+    component: TasksExtraByDate,
+  },
+  {
+    path: '/testing',
+    name: ROUTE_NAME_TESTING,
+    component: Testing,
+  },
+  {
+    path: '/received-tasks-by-period',
+    name: ROUTE_NAME_RECEIVED_TASKS_BY_PERIOD,
+    component: ReceivedTasksByPeriod,
   },
   {
     path: '/:pathMatch(.*)*',

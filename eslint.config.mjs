5ff7cc0b65a20b385b2adf08dbe6a95fbf47e6b9
Vfif <gmariya6666@gmail.com>
2025-06-23 14:34:30 +0300

    Обновлены стили BaseTable

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
 import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import pluginVue from "eslint-plugin-vue";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import pluginVue from "eslint-plugin-vue";
-import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([

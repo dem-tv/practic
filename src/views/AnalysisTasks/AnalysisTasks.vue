b9241e94054b5ea114f0bc833a70ffcaf71cf5c0
Vfif <gmariya6666@gmail.com>
2025-06-24 14:06:33 +0300

    Обновлен компонент AnalysisTasks

--- src/views/AnalysisTasks/AnalysisTasks.vue
+++ src/views/AnalysisTasks/AnalysisTasks.vue
@@ -1,1 +1,73 @@
-<template></template>
+<script setup lang="ts">
+import { helpers, required } from '@vuelidate/validators';
+
+import { getMessageRequired } from '@/utils/getMessage';
+
+import type { DateModel } from '@/types/datePeriod.types';
+
+import {
+  DATE_FNS_FORMAT_ISO_WITH_TIMEZONE,
+  DATE_FORMAT_ISO_WITH_TIMEZONE,
+} from '@/constants/dates';
+import { ROUTE_NAME_MAIN } from '@/constants/routeNames';
+
+import { useValidation } from '@/composable/useValidation';
+
+const LABEL_DATE = 'Дата';
+
+type FromModel = {
+  date: DateModel;
+};
+
+const formModel = ref<FromModel>({
+  date: null,
+});
+
+const validationRules = computed(() => ({
+  date: {
+    required: helpers.withMessage(getMessageRequired(LABEL_DATE), required),
+  },
+}));
+
+const { formValidation, validateForm } = useValidation(formModel, validationRules);
+
+async function onShow() {
+  const isValid = await validateForm();
+  if (!isValid) return;
+
+  console.log(formModel.value);
+}
+
+async function onPrepare() {
+  const isValid = await validateForm();
+  if (!isValid) return;
+
+  console.log(formModel.value);
+}
+</script>
+
+<template>
+  <TitledContent
+    :link-to-back="{ name: ROUTE_NAME_MAIN }"
+    title="Заявки на анализе"
+  >
+    <BaseForm>
+      <DatePeriod
+        v-model="formModel.date"
+        picker-type="point"
+        :format-date="DATE_FNS_FORMAT_ISO_WITH_TIMEZONE"
+        :format-dayjs="DATE_FORMAT_ISO_WITH_TIMEZONE"
+        :label="LABEL_DATE"
+        :error-message="formValidation.date.errorMessage"
+        :error="formValidation.date.invalid"
+        :required="formValidation.date.required"
+        placeholder="Выберите дату"
+        name="date"
+      />
+      <HorizontalList>
+        <BaseButton @click="onShow"> Посмотреть </BaseButton>
+        <BaseButton @click="onPrepare"> Подготовить к загрузке </BaseButton>
+      </HorizontalList>
+    </BaseForm>
+  </TitledContent>
+</template>
